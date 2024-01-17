ba59958018912dd50dc1410755033adc9be38be9
Cleiton <cleiton.siqueira.266@gmail.com>
2024-01-16 23:21:32 -0300

    Initial commit

---
+++ API/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Oi";
+        }
+    }
+}
