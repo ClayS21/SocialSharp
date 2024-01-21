5f75b386b2ab8cbd463c37ddddf774ffad125d0f
Cleiton <cleiton.siqueira.266@gmail.com>
2024-01-20 23:35:26 -0300

    Client app added

--- API/Startup.cs
+++ API/Startup.cs
@@ -22,6 +22,7 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +36,9 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(policy => 
+            policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
