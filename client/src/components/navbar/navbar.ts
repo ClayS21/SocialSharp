f9aa2acfcb01d5d8de53184e61aa70aee4c35c61
Cleiton <cleiton.siqueira.266@gmail.com>
2025-10-20 13:38:36 -0300

    Add default image for user

--- client/src/components/navbar/navbar.ts
+++ client/src/components/navbar/navbar.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'navbar',
-  imports: [],
-  templateUrl: './navbar.html',
-  styleUrl: './navbar.css'
+    selector: 'navbar',
+    imports: [FormsModule],
+    templateUrl: './navbar.html',
+    styleUrl: './navbar.css'
 })
 export class Navbar {
 
